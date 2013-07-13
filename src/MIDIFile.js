3dda851fb649371d0c80415fd3105f37613c6d41
Nicolas Froidure <nicolas_froidure@elitwork.com>
2013-07-13 11:47:16 +0200

    MIDIFile now has the logic to read lyrics and MIDI events

--- src/MIDIFile.js
+++ src/MIDIFile.js
@@ -35,6 +35,94 @@
 			throw new Error('It seems that the buffer contains too much datas.');
 	}
 
+	MIDIFile.prototype.getMidiEvents = function() {
+		// Reading events
+		var events, event, tempo=500, playTime=0, midiEvents=[],
+			format=this.header.getFormat(),
+			tickResolution=this.header.getTickResolution();
+		for(var i=0, j=this.tracks.length; i<j; i++) {
+			// reset playtime if format is 2
+			playTime=(2==format&&playTime?playTime:0);
+			events=new MIDIEvents.createParser(this.tracks[i].getTrackEvents(),0,false);
+			// loooping throught events
+			 while(event=events.next()) {
+			 	playTime+=(event.delta?event.delta*tickResolution:0);
+				if(event.type===MIDIEvents.EVENT_META) {
+					// tempo change events
+					if(event.subtype===MIDIEvents.EVENT_META_SET_TEMPO) {
+						tickResolution=this.header.getTickResolution(event.tempo);
+					}
+				// push midi events
+				} if(event.type===MIDIEvents.EVENT_MIDI) {
+					event.playTime=playTime;
+					midiEvents.push(event);
+				}
+			}
+		}
+		midiEvents.sort(function(a,b) {
+			return (a.playTime<b.playTime?-1:(a.playTime>b.playTime?1:0));
+		});
+		return midiEvents;
+	}
+
+	MIDIFile.prototype.getLyrics = function() {
+		// Reading events
+		var events, event, tempo=500, playTime=0,
+			format=this.header.getFormat(),
+			tickResolution=this.header.getTickResolution(),
+			karaoke=-1, texts=[], lyrics=[];
+		for(var i=0, j=this.tracks.length; i<j; i++) {
+			// reset playtime if format is 2
+			playTime=(2==format&&playTime?playTime:0);
+			events=new MIDIEvents.createParser(this.tracks[i].getTrackEvents(),0,false);
+			// loooping throught events
+			 while(event=events.next()) {
+			 	playTime+=(event.delta?event.delta*tickResolution:0);
+				if(event.type===MIDIEvents.EVENT_META) {
+					// tempo change events
+					if(event.subtype===MIDIEvents.EVENT_META_SET_TEMPO) {
+						tickResolution=this.header.getTickResolution(event.tempo);
+					// lyrics
+					} else if(event.subtype===MIDIEvents.EVENT_META_LYRICS) {
+							event.playTime=playTime;
+							lyrics.push(event);
+					} else if(event.subtype===MIDIEvents.EVENT_META_TEXT) {
+						// karaoke detection
+						if(i<karaoke+2&&karaoke>0&&event.text) {
+							// KAR file
+							// Special text
+							if(event.text[0]=='@') {
+								if(event.text[1]=='T') {
+									//console.log('Title : '+event.text.substring(2));
+								} else if(event.text[1]=='I') {
+									//console.log('Info : '+event.text.substring(2));
+								} else if(event.text[1]=='L') {
+									//console.log('Lang : '+event.text.substring(2));
+								}
+							// karaoke text follows, remove all previous text
+							} else if(0===event.text.indexOf('words')) {
+								texts.length=0;
+								//console.log('Word marker found');
+							} else {
+								event.playTime=playTime;
+								texts.push(event);
+							}
+						} else if(1==format&&event.text&&i<3&&0===event.text.indexOf('@K')) {
+							karaoke=i;
+							//console.log('Karaoke. Track: '+karaoke)
+						}
+					}
+				}
+			}
+		}
+		if(lyrics.length) {
+			return lyrics;
+		} else if(texts.length) {
+			return texts;
+		}
+		return [];
+	};
+
 // END: Module logic end
 
 	return MIDIFile;
